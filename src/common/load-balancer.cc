c216353fb47e7a15ed139c4eb29690f62ec91f45
jdziura <jdziura10@gmail.com>
2024-01-19 13:29:51 +0100

    Fix job-manager and load-balancer interface

--- src/common/load-balancer.cc
+++ src/common/load-balancer.cc
@@ -1,3 +1,6 @@
+#include <thread>
+#include <iostream>
+
 #include "load-balancer.h"
 
 namespace mapreduce {
@@ -13,6 +16,21 @@ std::string LoadBalancer::get_worker_ip() {
  * TODO: implement
 */
 void LoadBalancer::notify_worker_finished(std::string const& worker_ip) {
+    return;
+}
+
+void LoadBalancer::start() {
+    while (true) {
+        std::cerr << "[LOAD BALANCER] Refreshing workers..." << std::endl;
+        refresh_workers();
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+}
+
+/**
+ * TODO: implement
+*/
+void LoadBalancer::refresh_workers() {
     return;
 }
 
