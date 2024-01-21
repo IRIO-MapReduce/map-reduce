302cc8b1e380d08c758975ba72f080f0df29a447
SzymonM213 <s.mrozicki123@gmail.com>
2024-01-21 21:35:06 +0100

    add grep mapper

---
+++ src/test_mapper/grep_mapper.cc
@@ -0,0 +1,32 @@
+#include "../common/mapper.h"
+#include "../common/mapreduce.h"
+#include "../common/utils.h"
+
+class LongMapper : public mapreduce::Mapper {
+public:
+    void map() override
+    {
+        mapreduce::key_t key;
+        mapreduce::val_t val;
+
+        std::string word = "MapReduce";
+
+        while (get_next_pair(key, val)) {
+            std::cerr << "[LONG MAPPER] key: " << key << ", val: " << val
+                      << std::endl;
+            if (val.find(word) != std::string::npos) {
+                emit(key, val);
+            }
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    std::cerr << "[MAPPER WORKER] Starting binary" << std::endl;
+    LongMapper mapper;
+
+    mapper.start(argc, argv);
+
+    return 0;
+}
